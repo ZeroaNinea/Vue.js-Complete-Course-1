6672496cfb1ada3f364f34dc55a5dc5987f97973
Catgirl <zeroaninea@gmail.com>
2026-02-18 22:38:19 +0400

    contact-us imported

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import ContactUs from './components/ContactUs.vue'
+
 import router from './router'
 
 const app = createApp(App)
+
+app.component('contact-us', ContactUs)
 
 app.use(createPinia())
 app.use(router)
