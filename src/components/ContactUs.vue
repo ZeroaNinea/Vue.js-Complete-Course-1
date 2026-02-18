9690d0f9bdc828b68370851d560227b15a2bd638
Catgirl <zeroaninea@gmail.com>
2026-02-18 23:25:15 +0400

    email added

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,6 +1,22 @@
+<script setup lang="ts"></script>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const email = ref('hello@dotnetmastery.com')
+
+    return {
+      email,
+    }
+  },
+}
+</script>
+
 <template>
   <div>
     <h1>Contact Us</h1>
-    <p>Please send an email to hello@dotnetmastery.com</p>
+    <p>Please send an email to {{ email }}</p>
   </div>
 </template>
