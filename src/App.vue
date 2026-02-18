c4e4c35f45a38214af74c8c041b1b95c4e13e74b
Catgirl <zeroaninea@gmail.com>
2026-02-18 19:12:06 +0400

    display hello world

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts"></script>
 
+<script lang="ts">
+export default {
+  setup() {
+    const message = 'Hello World!'
+
+    return {
+      message,
+    }
+  },
+}
+</script>
+
 <template>
   <h1>You did it!</h1>
+  <h2>{{ message }}</h2>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
