10e017ea22e4f30fd89064a27f2cbe04e80d3291
Catgirl <zeroaninea@gmail.com>
2026-02-19 00:36:50 +0400

    the .container added

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,16 @@ const message = 'Hello World!'
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <h2>{{ message }}</h2>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <ContactUs></ContactUs>
-  <button-counter></button-counter>
+  <div class="container">
+    <h1>You did it!</h1>
+    <h2>{{ message }}</h2>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+    <ContactUs></ContactUs>
+    <button-counter></button-counter>
+  </div>
 </template>
 
 <style scoped></style>
