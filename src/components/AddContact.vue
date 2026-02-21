dc3afaab80709c189b1d4e000364126505284771
Catgirl <zeroaninea@gmail.com>
2026-02-22 02:07:32 +0400

    AddContact.vue created

---
+++ src/components/AddContact.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const contact = reactive({
+  name: '',
+  phone: '',
+  email: '',
+})
+
+function addContact() {
+  console.log(contact)
+  contact.name = ''
+  contact.phone = ''
+  contact.email = ''
+}
+</script>
+
+<template>
+  <div class="container border rounded p-2">
+    <div class="text-center text-white">Add New Contact</div>
+    <form @submit.prevent="addContact">
+      <div class="row">
+        <div class="col-4">
+          <input v-model="contact.name" placeholder="Enter Name" type="text" class="form-control" />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
